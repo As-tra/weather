0506a7aa80ea2e3b30aca0cbfb464aed84b30a6b
Assil <astradhdbjd@gmail.com>
2024-01-25 22:44:23 +0100

    newer version

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,6 +1,9 @@
-class WeatherService {
+import 'package:http/http.dart' as http;
 
-  void getWeather({required cityName}) {
-    
+class WeatherService {
+  void getWeather({required cityName}) async {
+    // you reach video 14
+    http.Response response = await http.get(Uri.parse(
+        'http://api.weatherapi.com/v1/current.json?key=79df5cfa03cf4041924180055242301&q=London&aqi=no'));
   }
 }
